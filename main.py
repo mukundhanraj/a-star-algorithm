bd59994124a74d9b0c19f0e1ec6d080c369274da
Rishabh Mukund <rishabh.m96@gmail.com>
2022-03-21 00:14:14 -0400

    fixed bugs while taking inputs

--- main.py
+++ main.py
@@ -6,52 +6,59 @@
 
 import map
 import astar
-import numpy as np
 
 if __name__ == '__main__':
     map_len = 400
     map_bre = 250
     thresh = 1.5
     flag = True
+    validPoints = []
+    radius = 0
+    clearance = 0
+    thresh = 0
 
     radius = input('Input the radius of the robot:\n')
+    radius = int(radius)
     if radius < 1:
         flag = False
         print('Radius must be positive, please try again...')
 
     if flag:
         clearance = input('Input the clearance:\n')
+        clearance = int(clearance)
     if clearance < 0:
         flag = False
         print('clearance must be positve or 0, please try again...')
 
     if flag:
-        print('Please wait...')
         validPoints = map.listOfValidPoints(map_len, map_bre, radius)
 
     if flag:
         start = input('Input Staring Position in format: x,y,th\n')
-        start = np.int0(start.split(','))
+        start = (int(start.split(',')[0]), int(
+            start.split(',')[1]), int(start.split(',')[2]))
         if not map.isPointValid(start[0:2], validPoints, clearance):
             flag = False
             print('Not a valid point, please try again...')
 
     if flag:
         goal = input('Input Staring Position in format: x,y,th\n')
-        goal = np.int0(goal.split(','))
+        goal = (int(goal.split(',')[0]), int(
+            goal.split(',')[1]), int(goal.split(',')[2]))
         if not map.isPointValid(goal[0:2], validPoints, clearance):
             flag = False
             print('Not a valid point, please try again...')
 
     if flag:
         step = input('Input the step size for robot:\n')
+        step = int(step)
         if step < 1:
             flag = False
             print('not a valid step size, please try again...')
 
     if flag:
         print('starting')
-        reached, parent_map, closed = astar(
+        reached, parent_map, closed = astar.astar(
             start, goal, validPoints, clearance, step, thresh)
         if reached:
             print('reached')
